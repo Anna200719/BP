ad99b9fd0e0210b281b36bd3afa497a759a19ebb
Anna200719 <73298456+Anna200719@users.noreply.github.com>
2020-12-18 17:06:47 +0200

    Added root as dirname

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import path from 'path';
 import router from './router';
+/* eslint no-underscore-dangle: 0 */
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser());
 
 const PORT = 3000;
 
-app.use('/', express.static('/Users/annaduchovny/Desktop/XiomaHomeWork/BP/src/client'));
+app.use(express.static(path.join(__dirname, 'src/client')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
